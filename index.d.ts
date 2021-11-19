431a76af66d7300a6340eb818d1d683535fa56d4
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2021-11-19 00:27:40 -0600

    Add typings cuz why not

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+declare module 'utils' {
+  interface DateUtils {
+    parseDate(isoDate: string | number | Date, offset?: number): string;
+  }
+
+  interface FunctionUtils {
+    applyBind<T extends (this: unknown, ...args: unknown[]) => unknown>(fn: T, ...argsArray?: Parameters<T>): (thisArg?: ThisParameterType<T>, argsArray?: Parameters<T>) => ReturnType<T>;
+    uncurryThis<T extends (this: unknown, ...args: unknown[]) => unknown>(fn: T, ...argsArray?: Parameters<T>): (thisArg?: ThisParameterType<T>, ...args?: Parameters<T>) => ReturnType<T>;
+  }
+
+  interface NumberUtils {
+    numberWithCommas(mun?: number): string;
+  }
+
+  interface ObjectUtils {
+    clone<T>(obj: T): T;
+    getProperties(obj: any): PropertyKey[];
+    getPropertyDescriptor(obj: any, prop: PropertyKey): PropertyDescriptor;
+    getPropertyNames(obj: any): string[];
+    getPropertySymbols(obj: any): symbol[];
+    getPrototypeChain(obj: any): (string | undefined)[];
+    hasProperty(obj: any, prop: PropertyKey): boolean;
+  }
+
+  interface StringUtils {
+    toArgs(str?: string): string[];
+  }
+
+  export {
+    DateUtils,
+    FunctionUtils,
+    NumberUtils,
+    ObjectUtils,
+    StringUtils
+  }
+}
