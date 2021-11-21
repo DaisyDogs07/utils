70d35341a0e24df7c21bad66d77dc4e7bc140484
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2021-11-21 09:23:05 -0600

    Swap `interface` with `const`

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
 declare module 'utils' {
-  export interface DateUtils {
+  export const DateUtils: {
     parseDate(isoDate: string | number | Date, offset?: number): string;
   }
 
-  export interface FunctionUtils {
+  export const FunctionUtils: {
     applyBind<T extends (...args: any[]) => void>(fn: T, ...argsArray: Parameters<T>): (thisArg?: ThisType<T>, argsArray?: Parameters<T>) => ReturnType<T>;
     uncurryThis<T extends (...args: any[]) => void>(fn: T, ...argsArray: Parameters<T>): (thisArg?: ThisType<T>, ...args: Parameters<T>) => ReturnType<T>;
   }
 
-  export interface NumberUtils {
+  export const NumberUtils: {
     numberWithCommas(num?: number): string;
   }
 
-  export interface ObjectUtils {
+  export const ObjectUtils: {
     clone<T>(obj: T): T;
     getProperties(obj: any): PropertyKey[];
     getPropertyDescriptor(obj: any, prop: PropertyKey): PropertyDescriptor;
@@ -22,7 +22,7 @@ declare module 'utils' {
     hasProperty(obj: any, prop: PropertyKey): boolean;
   }
 
-  export interface StringUtils {
+  export const StringUtils: {
     toArgs(str?: string): string[];
   }
 }
