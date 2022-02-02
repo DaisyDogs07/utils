c3855f0b66305dc66dda3339bcfb48953cb7ab6d
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2022-02-02 09:19:32 -0600

    Chenges

--- index.d.ts
+++ index.d.ts
@@ -1,50 +1,25 @@
 declare module 'utils' {
-  export const DateUtils: {
-    parseDate(isoDate: string | number | Date, offset?: number): string;
-  }
-
   export const FunctionUtils: {
-    applyBind<T extends (...args: any[]) => void>(fn: T): (thisArg?: ThisType<any>, argsArray?: Parameters<T>) => ReturnType<T>;
-    uncurryThis<T extends (...args: any[]) => void>(fn: T): (thisArg?: ThisType<any>, ...args: Parameters<T>) => ReturnType<T>;
+    applyBind<T extends (...args: any[]) => void>(fn: T): (thisArg?: any, argsArray?: Parameters<T>) => ReturnType<T>;
+    uncurryThis<T extends (...args: any[]) => void>(fn: T): (thisArg?: any, ...args?: Parameters<T>) => ReturnType<T>;
   }
 
   export const MathUtils: {
     average(...numbers: number[]): number;
-    average(numbers: number[]): number;
     clamp(value: number, min?: number, max?: number): number;
 
     distance(x1: number, x2: number): number;
-    distance(point1: number[], point2: number[]): number;
     distance(x1: number, x2: number, y1: number, y2: number): number;
     distance(x1: number, x2: number, y1: number, y2: number, z1: number, z2: number): number;
-    distance(...pairs: number[]): number;
 
     lerp(min: number, max: number, t?: number): number;
-    normalize(value: number, min?: number, max?: number): number;
-  }
-
-  export const NumberUtils: {
-    kMaxFractionDigits: number;
-    numberWithCommas(num?: number): string;
+    normalize(value: number, min: number, max: number): number;
     random(min?: number, max?: number): number;
-    realNumber(num?: number): string;
   }
 
   export const ObjectUtils: {
-    clone<T>(obj: T): T;
-    getProperties(obj: any): (string | symbol)[];
-    getPropertyDescriptor(obj: any, prop: PropertyKey): PropertyDescriptor;
-    getPropertyDescriptors(obj: any): {
-      [x: PropertyKey]: PropertyDescriptor;
+    clone<T>(obj: T): {
+      [P in keyof T]: T[P];
     };
-    getPropertyNames(obj: any): string[];
-    getPropertySymbols(obj: any): symbol[];
-    getPrototypeChain(obj: any): any[];
-    hasOwn(obj: any, prop: PropertyKey): boolean;
-    hasProperty(obj: any, prop: PropertyKey): boolean;
-  }
-
-  export const StringUtils: {
-    toArgs(str?: string): string[];
   }
 }
--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 const utils = {
-  DateUtils: (function DateUtils() {
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    function parseDate(isoDate = new Date().toISOString(), offset = 0) {
-      if (isoDate instanceof Date)
-        isoDate = isoDate.toISOString();
-      if (typeof isoDate === 'number')
-        isoDate = new Date(isoDate).toISOString();
-      if (offset)
-        isoDate = new Date(new Date(isoDate).getTime() + (36e5 * offset)).toISOString();
-      const date = isoDate.split('T')[0].split('-'),
-        time = isoDate.split('T')[1].replace('Z', '').split(':'),
-        amOrPm = +time[0] >= 12 ? 'PM' : 'AM',
-        hour = (amOrPm === 'AM' ? +time[0] : time[0] - 12) || 12;
-      return `${months[date[1] - 1]} ${date[2]}, ${date[0]} ${hour}:${time[1]}:${time[2]} ${amOrPm}`;
-    }
-
-    return {
-      parseDate
-    };
-  })(),
-  FunctionUtils: (function FunctionUtils() {
+  FunctionUtils: (function() {
     const {
       apply,
       bind,
@@ -31,189 +11,44 @@ const utils = {
       uncurryThis: bind.bind(call)
     };
   })(),
-  MathUtils: (function MathUtils() {
-    function average() {
+  MathUtils: {
+    average() {
       let avg = 0;
-      if (Array.isArray(arguments[0])) {
-        for (const n of arguments[0])
-          avg += +n;
-        return avg / arguments[0].length;
-      }
       for (const n of arguments)
         avg += +n;
       return avg / arguments.length;
-    }
-
-    function clamp(value, min = 0, max = 1) {
+    },
+    clamp(value, min = 0, max = 1) {
       if (value < min)
         return min;
       if (value > max)
         return max;
       return value;
-    }
-
-    function distance() {
+    },
+    distance() {
       let dist = 0;
-      if (Array.isArray(arguments[0]) &&
-          Array.isArray(arguments[1])) {
-        for (let i = 0; i < arguments[0].length && i < arguments[1].length; i++)
-          dist += (+arguments[0][i] - +arguments[1][i]) ** 2;
-        return dist ** 0.5;
-      }
-      for (let i = 1; i < arguments.length; i += 2)
-        dist += (+arguments[i - 1] - +arguments[i]) ** 2;
+      for (let i = 0; i < arguments.length; ++i)
+        dist += (+arguments[i] - +arguments[++i]) ** 2;
       return dist ** 0.5;
-    }
-
-    function lerp(min, max, t = 0) {
+    },
+    lerp(min, max, t = 0) {
       return t * (max - min) + min;
-    }
-
-    function normalize(value, min = 0, max = 1) {
+    },
+    normalize(value, min, max) {
       return (value - min) / (max - min);
-    }
-
-    return {
-      average,
-      clamp,
-      distance,
-      lerp,
-      normalize
-    };
-  })(),
-  NumberUtils: (function NumberUtils() {
-    let kMaxFractionDigits;
-    try {
-      (0).toFixed(100);
-      kMaxFractionDigits = 100;
-    } catch (e) {
-      kMaxFractionDigits = 20;
-    }
-
-    function numberWithCommas(num = 0) {
-      const arr = num.toString().split('.');
-      arr[0] = arr[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-      return arr.join('.');
-    }
-
-    function random(min = 0, max = 1) {
+    },
+    random(min = 0, max = 1) {
       return Math.random() * (max - min) + min;
     }
-
-    function realNumber(num = 0) {
-      if (Object.is(num, -0))
-        return '-0';
-      return num.toFixed(kMaxFractionDigits).replace(/\.?0+$/, '');
-    }
-
-    return {
-      kMaxFractionDigits,
-      numberWithCommas,
-      random,
-      realNumber
-    };
-  })(),
-  ObjectUtils: (function ObjectUtils() {
-    function clone(obj) {
+  },
+  ObjectUtils: {
+    clone(obj) {
       return Object.create(
         Object.getPrototypeOf(obj),
         Object.getOwnPropertyDescriptors(obj)
       );
     }
-
-    function getProperties(obj) {
-      const properties = [];
-      if (obj !== void 0 && obj !== null)
-        for (; obj !== null; obj = Object.getPrototypeOf(obj))
-          properties.push(
-            ...Reflect.ownKeys(obj).filter(v => !properties.includes(v))
-          );
-      return properties;
-    }
-
-    function getPropertyDescriptor(obj, prop) {
-      if (obj !== void 0 && obj !== null)
-        for (; obj !== null; obj = Object.getPrototypeOf(obj))
-          if (prop in obj)
-            return Object.getOwnPropertyDescriptor(obj, prop);
-    }
-
-    function getPropertyDescriptors(obj, prop) {
-      const descs = {};
-      if (obj !== void 0 && obj !== null)
-        for (; obj !== null; obj = Object.getPrototypeOf(obj)) {
-          const ownDescs = Object.getOwnPropertyDescriptors(obj);
-          for (const key in ownDescs)
-            if (key in descs)
-              delete ownDescs[key];
-          Object.assign(descs, ownDescs);
-        }
-      return descs;
-    }
-
-    function getPropertyNames(obj) {
-      const names = [];
-      if (obj !== void 0 && obj !== null)
-        for (; obj !== null; obj = Object.getPrototypeOf(obj))
-          names.push(...Object.getOwnPropertyNames(obj).filter(v => !names.includes(v)));
-      return names;
-    }
-
-    function getPropertySymbols(obj) {
-      const symbols = [];
-      if (obj !== void 0 && obj !== null)
-        for (; obj !== null; obj = Object.getPrototypeOf(obj))
-          symbols.push(...Object.getOwnPropertySymbols(obj).filter(v => !symbols.includes(v)));
-      return symbols;
-    }
-
-    function getPrototypeChain(obj) {
-      const chain = [];
-      if (obj !== void 0 && obj !== null)
-        for (obj = Object.getPrototypeOf(obj); obj !== null; obj = Object.getPrototypeOf(obj))
-          chain.push(obj);
-      return chain;
-    }
-
-    let hasOwn;
-    if (!('hasOwn' in Object)) {
-      const {
-        bind,
-        call
-      } = Function.prototype,
-        uncurryThis = bind.bind(call);
-      hasOwn = uncurryThis(Object.prototype.hasOwnProperty);
-    } else hasOwn = Object.hasOwn;
-
-    function hasProperty(obj, prop) {
-      if (obj !== void 0 && obj !== null)
-        for (; obj !== null; obj = Object.getPrototypeOf(obj))
-          if (prop in obj)
-            return true;
-      return false;
-    }
-
-    return {
-      clone,
-      getProperties,
-      getPropertyDescriptor,
-      getPropertyDescriptors,
-      getPropertyNames,
-      getPropertySymbols,
-      getPrototypeChain,
-      hasOwn,
-      hasProperty
-    };
-  })(),
-  StringUtils: (function StringUtils() {
-    function toArgs(str = '') {
-      return str.split(/ |\n/).filter(v => v);
-    }
-
-    return {
-      toArgs
-    };
-  })()
+  }
 };
 
 if (typeof window !== 'undefined')
