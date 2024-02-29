b959d0083683b2288e23a628f3bbe582ad0aa083
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2024-02-28 21:30:55 -0500

    add memory.js

---
+++ memory.js
@@ -0,0 +1,318 @@
+/**
+ * @type {ArrayBuffer[]}
+ */
+let pages = [new ArrayBuffer(65536)];
+/**
+ * @type {{ offset: bigint, size: bigint }[]}
+ */
+let allocedRegions = [];
+
+/**
+ * @param {bigint} address
+ * @returns {{ page: ArrayBuffer, offset: number }}
+ */
+function findPageIndexAndOffset(address) {
+  let pageIndex = 0;
+  let offset = address;
+  for (let i = 0; i < pages.length; ++i) {
+    if (offset < 65536n) {
+      pageIndex = i;
+      break;
+    }
+    offset -= 65536n;
+  }
+  return { page: pages[pageIndex], offset: Number(offset) };
+}
+
+/**
+ * @param {bigint} address
+ * @returns {number}
+ */
+function readInt8(address) {
+  let num = readUint8(address);
+  if (num >= 0x80)
+    num -= 0x100;
+  return num;
+}
+
+/**
+ * @param {bigint} address
+ * @returns {number}
+ */
+function readUint8(address) {
+  const { page, offset } = findPageIndexAndOffset(address);
+  return new DataView(page).getUint8(offset);
+}
+
+/**
+ * @param {bigint} address
+ * @param {number} value
+ */
+function writeInt8(address, value) {
+  writeUint8(address, value);
+}
+
+/**
+ * @param {bigint} address
+ * @param {number} value
+ */
+function writeUint8(address, value) {
+  const { page, offset } = findPageIndexAndOffset(address);
+  new DataView(page).setUint8(offset, value);
+}
+
+/**
+ * @param {bigint} address
+ * @returns {number}
+ */
+function readInt16(address) {
+  let num = readUint16(address);
+  if (num >= 0x8000)
+    num -= 0x10000;
+  return num;
+}
+
+/**
+ * @param {bigint} address
+ * @returns {number}
+ */
+function readUint16(address) {
+  return (((readUint8(address) << 8) >>> 0) | readUint8(address + 1)) >>> 0;
+}
+
+/**
+ * @param {bigint} address
+ * @param {number} value
+ */
+function writeInt16(address, value) {
+  writeUint16(address, value);
+}
+
+/**
+ * @param {bigint} address
+ * @param {number} value
+ */
+function writeUint16(address, value) {
+  if (value < 0)
+    value += 0x10000;
+  writeUint8(address, (value >> 8) & 0xFF);
+  writeUint8(address + 1, value & 0xFF);
+}
+
+/**
+ * @param {bigint} address
+ * @returns {number}
+ */
+function readInt32(address) {
+  let num = readUint32(address);
+  if (num >= 0x80000000)
+    num -= 0x100000000;
+  return num;
+}
+
+/**
+ * @param {bigint} address
+ * @returns {number}
+ */
+function readUint32(address) {
+  return (((readUint16(address) << 16) >>> 0) | readUint16(address + 2)) >>> 0;
+}
+
+/**
+ * @param {bigint} address
+ * @param {number} value
+ */
+function writeInt32(address, value) {
+  writeUint32(address, value);
+}
+
+/**
+ * @param {bigint} address
+ * @param {number} value
+ */
+function writeUint32(address, value) {
+  if (value < 0)
+    value += 0x100000000;
+  writeUint16(address, (value >> 16) & 0xFFFF);
+  writeUint16(address + 2, value & 0xFFFF);
+}
+
+/**
+ * @param {bigint} address
+ * @returns {bigint}
+ */
+function readInt64(address) {
+  let num = readUint64(address);
+  if (num >= 0x8000000000000000n)
+    num -= 0x10000000000000000n;
+  return num;
+}
+
+/**
+ * @param {bigint} address
+ * @returns {bigint}
+ */
+function readUint64(address) {
+  return (BigInt(readUint32(address)) << 32n) | BigInt(readUint32(address + 4));
+}
+
+/**
+ * @param {bigint} address
+ * @param {bigint} value
+ */
+function writeInt64(address, value) {
+  writeUint64(address, value);
+}
+
+/**
+ * @param {bigint} address
+ * @param {bigint} value
+ */
+function writeUint64(address, value) {
+  if (value < 0n)
+    value += 0x10000000000000000n;
+  writeUint32(address, Number((value >> 16n) & 0xFFFFFFFFn));
+  writeUint32(address + 4, Number(value & 0xFFFFFFFFn));
+}
+
+/**
+ * @param {bigint} size
+ * @returns {bigint}
+ */
+function findBestFit(size) {
+  let bestFit = null;
+  let smallestSize = BigInt(Number.MAX_VALUE);
+  const firstRegion = allocedRegions[0] || { offset: BigInt(Number.MAX_VALUE) };
+  if (size < firstRegion.offset)
+    return 0n;
+  for (let i = 0; i != allocedRegions.length; ++i) {
+    const region = allocedRegions[i];
+    const nextRegion = allocedRegions[i + 1] || { offset: BigInt(Number.MAX_VALUE) };
+    if ((region.offset + region.size) + size <= nextRegion.offset) {
+      const size2 = nextRegion.offset - (region.offset + region.size);
+      if (smallestSize > size2 || (size2 === BigInt(Number.MAX_VALUE) && bestFit === null)) {
+        bestFit = region.offset + region.size;
+        smallestSize = size2;
+      }
+    }
+  }
+  return bestFit;
+}
+
+/**
+ * @param {bigint} size
+ * @returns {bigint}
+ */
+function malloc(size) {
+  size = BigInt(size);
+  let bestFit = findBestFit(size)
+  let totalSize = 65536n * BigInt(pages.length);
+
+  while (bestFit + size > totalSize) {
+    pages.push(new ArrayBuffer(65536));
+    totalSize += 65536;
+  }
+
+  let insertIndex = 0;
+  for (let i = 0; i < allocedRegions.length; i++) {
+    if (allocedRegions[i].offset > bestFit) {
+      insertIndex = i;
+      break;
+    }
+  }
+  allocedRegions.splice(insertIndex, 0, {
+    offset: bestFit,
+    size
+  });
+
+  return bestFit;
+}
+
+/**
+ * @param {bigint} address
+ */
+function free(address) {
+  address = BigInt(address);
+  const i = allocedRegions.findIndex(region => region.offset === address);
+  if (i === -1)
+    return;
+  if (i === allocedRegions.length - 1)
+    while (pages.length != 1 && allocedRegions[i - 1].offset + allocedRegions[i - 1].size <= (65536 * (pages.length - 1)))
+      pages.pop();
+  allocedRegions.splice(i, 1);
+}
+
+/**
+ * @param {bigint} address
+ * @param {bigint} size
+ * @returns {bigint}
+ */
+function realloc(address, size) {
+  address = BigInt(address);
+  size = BigInt(size);
+  const i = allocedRegions.findIndex(region => region.offset === address);
+  if (i === -1)
+    return;
+  const currentRegion = allocedRegions[i];
+  const nextRegion = allocedRegions[i + 1] || { offset: BigInt(Number.MAX_VALUE) };
+  if (currentRegion.offset + size <= nextRegion.offset) {
+    currentRegion.size = size;
+    return address;
+  }
+  const newAddr = malloc(size);
+  let bytesToCopy = currentRegion.size;
+  let destOff = newAddr;
+  let srcOff = address;
+  while (bytesToCopy / 8 >= 1) {
+    writeUint64(destOff, readUint64(srcOff));
+    destOff += 8;
+    srcOff += 8;
+    bytesToCopy -= 8;
+  }
+  if (bytesToCopy / 4 >= 1) {
+    writeUint32(destOff, readUint32(srcOff));
+    destOff += 4;
+    srcOff += 4;
+    bytesToCopy -= 4;
+  }
+  if (bytesToCopy / 2 >= 1) {
+    writeUint16(destOff, readUint16(srcOff));
+    destOff += 2;
+    srcOff += 2;
+    bytesToCopy -= 2;
+  }
+  if (bytesToCopy >= 1) {
+    writeUint8(destOff, readUint8(srcOff));
+    ++destOff;
+    ++srcOff;
+    --bytesToCopy;
+  }
+  free(address);
+  return newAddr;
+}
+
+const exports = {
+  malloc,
+  free,
+  realloc,
+  readInt8,
+  readUint8,
+  readInt16,
+  readUint16,
+  readInt32,
+  readUint32,
+  readInt64,
+  readUint64,
+  writeInt8,
+  writeUint8,
+  writeInt16,
+  writeUint16,
+  writeInt32,
+  writeUint32,
+  writeInt64,
+  writeUint64,
+};
+
+if (typeof window !== 'undefined')
+  Object.assign(window, exports);
+else module.exports = exports;
