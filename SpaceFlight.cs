19baad8f9ad0c5134b01a67aad865201eddc6c14
DaisyDogs07 <daisydogs2007@gmail.com>
2024-12-01 20:32:43 -0600

    Update SpaceFlight.cs

--- SpaceFlight.cs
+++ SpaceFlight.cs
@@ -1,4 +1,3 @@
-using System;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -6,26 +5,31 @@ using VRC.SDKBase;
 public class SpaceFlight : UdonSharpBehaviour {
   private VRCPlayerApi player;
   private bool isInCollider = false;
-  private Vector2 movement = new Vector2(0.0f, 0.0f);
+  private Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
   public float speed = 5.0f;
-  public float accelerationSmoothness = 0.225f;
-  public float decelerationSmoothness = 0.35f;
-  public float gravity = 0.001f;
+  public float acceleration = 0.225f;
+  public float deceleration = 0.35f;
+  public float gravity = 0.01f;
   public GameObject[] col;
 
-  void Start() {
+  private void Start() {
     player = Networking.LocalPlayer;
   }
 
-  void InputMoveVertical(float value, VRC.Udon.Common.UdonInputEventArgs args) {
-    movement.y = value;
+  private void InputMoveVertical(float value, VRC.Udon.Common.UdonInputEventArgs args) {
+    movement.z = value;
   }
 
-  void InputMoveHorizontal(float value, VRC.Udon.Common.UdonInputEventArgs args) {
+  private void InputMoveHorizontal(float value, VRC.Udon.Common.UdonInputEventArgs args) {
     movement.x = value;
   }
 
-  private void Update() {
+  private void InputLookVertical(float value, VRC.Udon.Common.UdonInputEventArgs args) {
+    if (player.IsUserInVR())
+      movement.y = value;
+  }
+
+  private void FixedUpdate() {
     Vector3 pos = player.GetPosition();
     bool isInCol = false;
     foreach (GameObject obj in col) {
@@ -38,19 +42,37 @@ public class SpaceFlight : UdonSharpBehaviour {
       }
     }
     if (isInCol != isInCollider)
-      player.SetGravityStrength(isInCol ? Mathf.Clamp(gravity, 0.001f, 1.0f) : 1.0f);
+      player.SetGravityStrength(isInCol ? 0.0f : 1.0f);
     isInCollider = isInCol;
-  }
-
-  private void FixedUpdate() {
     if (!isInCollider)
       return;
-    player.Immobilize(movement != Vector2.zero);
-    float adjustedSpeed = Mathf.Clamp(speed, 0.01f, 10.0f);
-    Quaternion rotation = player.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+    if (!player.IsUserInVR()) {
+      bool up = Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Space);
+      bool down = Input.GetKey(KeyCode.Q) || (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift));
+      if (up) {
+        if (down)
+          movement.y = 0.0f;
+        else movement.y = 1.0f;
+      } else if (down)
+        movement.y = -1.0f;
+      else movement.y = 0.0f;
+    }
+    player.Immobilize(movement != Vector3.zero);
     Vector3 currentVelocity = player.GetVelocity();
-    Vector3 targetVelocity = (rotation * new Vector3(movement.x, 0.0f, movement.y)) * adjustedSpeed;
-    Vector3 smoothedVelocity = Vector3.Lerp(currentVelocity, targetVelocity, (1.0f - Mathf.Clamp01(currentVelocity.magnitude >= targetVelocity.magnitude ? accelerationSmoothness : decelerationSmoothness)) * Time.fixedDeltaTime);
+    Vector3 targetVelocity = (player.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation * new Vector3(movement.x, 0.0f, movement.z)) * speed;
+    targetVelocity.y += movement.y * speed;
+    targetVelocity = Vector3.ClampMagnitude(targetVelocity, speed);
+    Vector3 smoothedVelocity = Vector3.Lerp(
+      currentVelocity,
+      targetVelocity,
+      (1.0f - Mathf.Clamp01(
+        Vector3.Dot(targetVelocity - currentVelocity, currentVelocity) > 0 &&
+        Vector3.Dot(currentVelocity.normalized, targetVelocity.normalized) > 0
+          ? acceleration
+          : deceleration
+      )) * Time.fixedDeltaTime
+    );
+    smoothedVelocity.y -= gravity * Time.fixedDeltaTime;
     player.SetVelocity(smoothedVelocity);
   }
 }
